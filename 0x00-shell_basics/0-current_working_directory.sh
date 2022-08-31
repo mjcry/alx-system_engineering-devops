7beead789931693bc9e2097e94b977c898bb8c62
mjcry <mj.masilo.jack@gmail.com>
2022-08-31 06:45:29 -0700

    Real test two

--- 0x00-shell_basics/0-current_working_directory.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo Directors for item in * do if [-d$item] the echo $item done echo files for item in * do if [-f$item] then echo $item fi done why?
--- 0x00-shell_basics/README.md
+++ 0x00-shell_basics/README.md
@@ -1,2 +1,2 @@
 #!/bin/bash
-#0-current_working_directory shebang is used to make an output.
+#0-current_working_directory pwd.
