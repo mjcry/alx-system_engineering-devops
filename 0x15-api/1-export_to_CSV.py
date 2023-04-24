c9e8aba0e273a108c06b1cbccdd1f07c117a1a1e
mjcry <mj.masilo.jack@gmail.com>
2023-04-24 06:42:19 -0700

    clear

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,38 @@
- Python script that, using this REST API,
+#!/usr/bin/python3
+"""
+Write a Python script that, using this REST API,
 for a given employee ID, returns information about
 his/her TODO list progress
 export data in the CSV format.
 """
+
 import csv
 import requests
 import sys
 
-"""
-import 
+
 if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: {} employee_id".format(sys.argv[0]))
+        sys.exit(1)
+
     id_c = sys.argv[1]
-    url_user = "https://jsonplaceholder.typicode.com/users/" + id_c
-    res = requests.get(url_user).json()
-    username = res.get("username")
-    req = requests.get(
-        'https://jsonplaceholder.typicode.com/users/' +
-        (id_c) + '/todos')
-    with open("{}.csv".format(sys.argv[1]), "w") as file_c:
+    url_user = "https://jsonplaceholder.typicode.com/users/{}".format(id_c)
+    res = requests.get(url_user)
+    if res.status_code != 200:
+        print("User not found")
+        sys.exit(1)
+    username = res.json().get("username")
+
+    url_task = "https://jsonplaceholder.typicode.com/todos"
+    res_task = requests.get(url_task, params={"userId": id_c})
+    if res_task.status_code != 200:
+        print("Tasks not found")
+        sys.exit(1)
+
+    with open("{}.csv".format(id_c), "w") as file_c:
         writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
-        for task in req.json():
-            writer.writerow([id_c, username,
-                            task.get("completed"), task.get("title")])
+        writer.writerow(["employee_id", "username", "completed", "title"])
+        for task in res_task.json():
+            writer.writerow([id_c, username, task.get("completed"), task.get("title")])
 
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,30 +1,43 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API,
+Write a  Python script that, using this REST API,
 for a given employee ID, returns information about
 his/her TODO list progress
-export data in the json format.
 """
-import json
-import requests
-import sys
 
-import json
 import requests
 import sys
 
 
 if __name__ == '__main__':
-    id_c = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                         .format(id_c)).json()
-    todos = requests.get("https://jsonplaceholder.typicode.com/todos?userId={}"
-                         .format(sys.argv[1])).json()
+    if len(sys.argv) != 2:
+        print("Usage: {} employee_id".format(sys.argv[0]))
+        sys.exit(1)
 
-    with open("{}.json".format(id_c), "w") as user_id:
-        json.dump({id_c: [{
-                'task': task.get('title'),
-                'completed': task.get('completed'),
-                'username': users.get('username')
-            } for task in todos]}, user_id)
+    id_c = sys.argv[1]
+    url_user = "https://jsonplaceholder.typicode.com/users/{}".format(id_c)
+    res = requests.get(url_user)
+    if res.status_code != 200:
+        print("User not found")
+        sys.exit(1)
+    name = res.json().get('name')
+
+    url_task = "https://jsonplaceholder.typicode.com/todos"
+    res_task = requests.get(url_task, params={"userId": id_c})
+    if res_task.status_code != 200:
+        print("Tasks not found")
+        sys.exit(1)
+
+    task_title = []
+    complete = 0
+    total_task = len(res_task.json())
+
+    for task in res_task.json():
+        if task.get('completed'):
+            task_title.append(task['title'])
+            complete += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(name, complete, total_task))
+    for title in task_title:
+        print("\t{}".format(title))
+
