ffa04027e39f5975872e9cd4d41b537b7f1f358d
mjcry <mj.masilo.jack@gmail.com>
2023-04-24 08:13:20 -0700

    gg

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,38 +1,37 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API,
-for a given employee ID, returns information about
-his/her TODO list progress
-export data in the CSV format.
+    export data in the CSV format
 """
 
-import csv
-import requests
-import sys
+if __name__ == "__main__":
+    import csv
+    import json
+    from sys import argv
+    import requests
 
+    # Data
+    ID = argv[1]
+    url_api = "https://jsonplaceholder.typicode.com"
 
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: {} employee_id".format(sys.argv[0]))
-        sys.exit(1)
+    # Endpoints
+    ep_user = "{}/users/{}".format(url_api, str(ID))
+    ep_todos = "{}/todos".format(ep_user)
 
-    id_c = sys.argv[1]
-    url_user = "https://jsonplaceholder.typicode.com/users/{}".format(id_c)
-    res = requests.get(url_user)
-    if res.status_code != 200:
-        print("User not found")
-        sys.exit(1)
-    username = res.json().get("username")
+    # Requests
+    user = requests.get(ep_user)
+    todos = requests.get(ep_todos)
 
-    url_task = "https://jsonplaceholder.typicode.com/todos"
-    res_task = requests.get(url_task, params={"userId": id_c})
-    if res_task.status_code != 200:
-        print("Tasks not found")
-        sys.exit(1)
+    # Format
+    user = user.json()
+    todos = todos.json()
 
-    with open("{}.csv".format(id_c), "w") as file_c:
-        writer = csv.writer(file_c, quoting=csv.QUOTE_ALL)
-        writer.writerow(["employee_id", "username", "completed", "title"])
-        for task in res_task.json():
-            writer.writerow([id_c, username, task.get("completed"), task.get("title")])
-
+    # Export to .csv file
+    with open(ID + ".csv", "w", encoding="utf8") as file:
+        writer = csv.writer(file, delimiter=",", quotechar="'")
+        for task in todos:
+            writer.writerow([
+                json.dumps(ID),
+                json.dumps(user.get("username")),
+                json.dumps(str(task.get("completed"))),
+                json.dumps(task.get("title"))
+            ])
